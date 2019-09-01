43ce758399c132ace6386a95578530642c2033aa
Garland Kent <galnad@Garlands-MacBook-Pro.local>
2019-09-01 00:19:20 -0600

    New update style

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Check for Updates
+#https://github.com/Elexi-dev/paper-auto-1.14.4
+git checkout master
+git stash
 git pull
+git stash pop --quiet
 #Start Server
 java -Xms128M -Xmx${SERVER_MEMORY}M -Dterminal.jline=false -Dterminal.ansi=true -jar paper.jar
-this is a random test so I can figure out the wondeful git
